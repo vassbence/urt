756cd86a090e0bb1885b4d1be64e48f7457987a4
Vass Bence <49574140+vassbence@users.noreply.github.com>
2026-08-14 02:42:16 +0200

    fix: example

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,24 @@
 #include <sstream>
 #include <urt.h>
 
+std::string escape_json(std::string_view value) {
+  std::string result;
+  for (char c : value) {
+    if (c == '"') {
+      result += "\\\"";
+    } else if (c == '\n') {
+      result += "\\n";
+    } else if (c == '\r') {
+      result += "\\r";
+    } else if (c == '\\') {
+      result += "\\\\";
+    } else {
+      result += c;
+    }
+  }
+  return result;
+}
+
 int main() {
   urt::run([](std::string_view event) {
     std::ostringstream response;
@@ -8,7 +26,7 @@ int main() {
              << "\"statusCode\": 200,\n"
              << "\"headers\": { \"Content-Type\": \"application/json\" },\n"
              << "\"isBase64Encoded\": false,\n"
-             << "\"body\": \"" << event << "\"\n"
+             << "\"body\": \"" << escape_json(event) << "\"\n"
              << "}";
     return response.str();
   });
